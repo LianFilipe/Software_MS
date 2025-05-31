a2901abb52395949d591ec0b438456686a705475
lian.nascimento <lianfilipesantanan@gmail.com>
2025-05-31 17:26:57 -0300

    First Commit Project

---
+++ src/main/java/com/example/Software_MS/SoftwareMsApplication.java
@@ -0,0 +1,13 @@
+package com.example.Software_MS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftwareMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftwareMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Software_MS/SoftwareMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Software_MS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftwareMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
